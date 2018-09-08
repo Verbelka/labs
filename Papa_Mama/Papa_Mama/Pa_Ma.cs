cdbb144d81b0ce4862f83e74f622a46891bb453b
Verbelka <32334022+Verbelka@users.noreply.github.com>
2018-09-08 17:45:21 +0300

    Lab about processes is added

---
+++ Papa_Mama/Papa_Mama/Pa_Ma.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Papa_Mama
+{
+    class Pa_Ma
+    {
+        string Str;
+        int amount;
+        int time;
+        public static int Messages { get; set; }
+
+        public Pa_Ma(string s, int a, int t)
+        {
+            this.Str = s;
+            this.amount = a;
+            this.time = t;
+        }
+
+        public void Work()
+        {
+            while(true)
+            {
+                if (amount != 0 && Messages != 0)
+                {
+                    amount--;
+                    Messages--;
+                    Thread.Sleep(time);
+                    Console.WriteLine(Str);
+                }
+                else
+                    Thread.CurrentThread.Abort();
+            }
+        }
+
+    }
+}
---
+++ Papa_Mama/Papa_Mama/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Papa_Mama
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Mess;
+            Console.WriteLine("Введите общее количество повторений : ");
+            Mess = Int32.Parse(Console.ReadLine());
+            Pa_Ma.Messages = Mess;
+
+            Pa_Ma s1 = new Pa_Ma("мяу ", 5, 1359);
+            Pa_Ma s2 = new Pa_Ma("гав ", 4, 2251);
+            Pa_Ma s3 = new Pa_Ma("пиу ", 2, 1987);
+
+            Thread t1 = new Thread(s1.Work);
+            Thread t2 = new Thread(s2.Work);
+            Thread t3 = new Thread(s3.Work);
+
+            t1.Start();
+            t2.Start();
+            t3.Start();
+        }
+    }
+}
